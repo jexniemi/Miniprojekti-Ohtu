e4b9c3610f7a73d25f867887c1c77dc1d10dd374
Katri <katsu4@hotmail.com>
2017-11-15 16:34:50 +0200

    form

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import InputForm from './components/InputForm.js';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello world!</h1>
+        < InputForm />
       </div>
     );
   }
