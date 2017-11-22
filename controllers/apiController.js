32f095c83b3b041db241551f5824111e423de1ea
thureson <tom.thureson@gmail.com>
2017-11-22 15:55:18 +0200

    db sending

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -12,7 +12,14 @@ module.exports = function (app) {
     });
 
     app.post("api/tips", function (req, res) {
-        res.send("TODO");
+        database.insertOne(req.body, function(err, doc) {
+          if (err) {
+            handleError(res, err.message, "Failed to create");
+          } else {
+            res.status(201).json(doc.ops[0]);
+          }
+        });
+        res.send("posted");
     });
 
     app.get("api/tips/:id", function (req, res) {
