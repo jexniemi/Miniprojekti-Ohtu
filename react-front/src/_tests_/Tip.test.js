b3eecccb73b948a478b19184968076327eb50eb3
thureson <tom.thureson@gmail.com>
2017-12-06 19:49:58 +0200

    Added tests

--- react-front/src/_tests_/Tip.test.js
+++ react-front/src/_tests_/Tip.test.js
@@ -13,5 +13,32 @@ it('renders without crashing', () => {
 
 test('Tip renders correct text', () => {
   const t = Enzyme.shallow(<Tip book={{ title: 'test', author: 'test' }} />);
-  expect(t.text()).toEqual('test: test edit');
+  expect(t.text()).toEqual('test: test ');
+});
+
+test('renders props correctly', () => {
+  const t = Enzyme.mount(<Tip book={{ title: 'test', author: 'test' }} />);
+  expect(t.props().book).toEqual({"author": "test", "title": "test"});
+});
+
+test('allows setting props', () => {
+  const t = Enzyme.mount(<Tip book={{ title: 'test', author: 'test' }} />);
+  t.setProps({ book: {"author": "test2", "title": "test2"}});
+  expect(t.props().book).toEqual({"author": "test2", "title": "test2"});
+});
+
+test('editing false by default', () => {
+  const t = Enzyme.mount(<Tip book={{ title: 'test', author: 'test' }} />);
+  expect(t.state().editing).toEqual(false);
+});
+
+test('editing changes when changeEditing is called', () => {
+  const t = Enzyme.mount(<Tip book={{ title: 'test', author: 'test' }} />);
+  t.instance().changeEditing();
+  expect(t.state().editing).toEqual(true);
+});
+
+test('edit-button initialized', () => {
+  const t = Enzyme.shallow(<Tip book={{ title: 'test', author: 'test' }} />);
+  expect(t.find('fa fa-pencil')).toHaveLength(0);
 });
--- react-front/src/components/BookList.js
+++ react-front/src/components/BookList.js
@@ -57,8 +57,8 @@ class BookList extends React.Component {
         return (
             <div>
                 <div className="PostForm">
-                <h2> Submit suggestions </h2>
-                    <SubmitForm refreshTips={this.addBook} />
+                    <h2> Submit suggestions </h2>
+                        <SubmitForm refreshTips={this.addBook} />
                 </div>
                 <div className="Tips" style={{ marginLeft: '5px' }}>
                     <h2> Book suggestions </h2>
@@ -74,7 +74,7 @@ class BookList extends React.Component {
                             }} type="text" placeholder="filter" />
                         </FormGroup>
                         <div style={{marginLeft: "15px"}}>
-                        {renderBooks}
+                            {renderBooks}
                         </div>
                     </div>
                 </div>
