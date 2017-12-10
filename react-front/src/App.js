032a918f6875353c95a02c3706397ef02aa4878e
woltsu <oltsuwh@gmail.com>
2017-12-10 13:03:32 +0200

    added video button to navbar and a possibility to browse between pages

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -4,12 +4,38 @@ import NavigationBar from './components/NavigationBar';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "books"
+    }
+
+    this.changeView = this.changeView.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  changeView(newView) {
+    console.log(newView);
+    this.setState({
+      view: newView
+    });
+  }
+
+  renderContent() {
+    return (
+      <div>
+        {this.state.view === "books" && <BookList />}
+        {this.state.view === "videos" && <p>VIDEOITA!!! :-)</p>}
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="Wrapper">
-        <NavigationBar className="Navbar" />   
+        <NavigationBar changeView={this.changeView} className="Navbar" />   
           <div className="Contents">
-            <BookList />
+            {this.renderContent()}
           </div>
       </div>
     );
--- react-front/src/components/NavigationBar.js
+++ react-front/src/components/NavigationBar.js
@@ -4,18 +4,34 @@ var Button = require('react-bootstrap').Button;
 var Navbar = require('react-bootstrap').Navbar;
 var FormControl = require('react-bootstrap').FormControl;
 var FormGroup = require('react-bootstrap').FormGroup;
+var Nav = require("react-bootstrap").Nav;
+var NavItem = require("react-bootstrap").NavItem;
 
 class NavigationBar extends React.Component {
     render() {
         return (
-            <Navbar>
+            <Navbar collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a className="NavHeader">BookBuddy</a>
+                        <a href="#">BookMate</a>
                     </Navbar.Brand>
+                    <Navbar.Toggle />
                 </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem style={styles.headerLink} onClick={() => this.props.changeView("books")}>Books</NavItem>
+                        <NavItem style={styles.headerLinkn} onClick={() => this.props.changeView("videos")}>Videos</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
+    }
+}
+
+const styles = {
+    headerLink: {
+        cursor: "pointer",
+        color: "white"
     }
 }
 
