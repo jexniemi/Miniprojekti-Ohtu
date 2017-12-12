328b562dfb178cafe8dda84585cd3dfd0f07d3a5
jexniemi <jxnieminen@gmail.com>
2017-12-12 16:43:16 +0200

    react-youtube added with sample video

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BookList from './components/BookList';
 import NavigationBar from './components/NavigationBar';
 import './App.css';
+import YouTube from 'react-youtube';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,9 +10,9 @@ class App extends React.Component {
     this.state = {
       view: "books"
     }
-
     this.changeView = this.changeView.bind(this);
     this.renderContent = this.renderContent.bind(this);
+
   }
 
   changeView(newView) {
@@ -22,10 +23,22 @@ class App extends React.Component {
   }
 
   renderContent() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    }
+
     return (
       <div>
         {this.state.view === "books" && <BookList />}
-        {this.state.view === "videos" && <p>VIDEOITA!!! :-)</p>}
+        {this.state.view === "videos" && <YouTube
+          videoId="2g811Eo7K8U"
+          opts={opts}
+          onReady={this._onReady}
+        />}
       </div>
     )
   }
@@ -33,10 +46,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="Wrapper">
-        <NavigationBar changeView={this.changeView} className="Navbar" />   
-          <div className="Contents">
-            {this.renderContent()}
-          </div>
+        <NavigationBar changeView={this.changeView} className="Navbar" />
+        <div className="Contents">
+          {this.renderContent()}
+        </div>
       </div>
     );
   }
